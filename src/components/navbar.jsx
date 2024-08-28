c0238603a4893d01b2c9c7a1a2ae9bd804b20f8f
Anubhav <singhanubhav7456@gmail.com>
2024-08-28 15:33:26 +0530

    mobile view nav buttons changed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -62,7 +62,7 @@ export function ExampleNavbarThree() {
         </div>
         <div className="hidden space-x-2 lg:block">
           <Link
-          to={"/contact"}>
+            to={"/contact"}>
             <button
               type="button"
               className="rounded-md border border-red-600 px-3 py-2 text-sm font-semibold text-red-600 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
@@ -115,18 +115,15 @@ export function ExampleNavbarThree() {
                   </nav>
                 </div>
                 <div className="mt-2 space-y-2">
-                  <button
-                    type="button"
-                    className="w-full rounded-md border border-black px-3 py-2 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-                  >
-                    Sign In
-                  </button>
-                  <button
-                    type="button"
-                    className="w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-                  >
-                    Log In
-                  </button>
+                  <Link 
+                  to={"/contact"}>
+                    <button
+                      type="button"
+                      className="w-full rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                    >
+                      Contact Us
+                    </button>
+                  </Link>
                 </div>
               </div>
             </div>
