95946bf172fa21cef38ccb43efe6b03e80a39c66
Palak Bhatnagar <160651549+PalakBhatnagar172@users.noreply.github.com>
2024-08-28 23:58:04 +0530

    about images

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,6 +4,15 @@ import React from 'react'
 import { Menu, X, MapPin } from 'lucide-react'
 import s2 from '../assets/s2.jpeg'
 import a2 from '../assets/a2.png'
+import priyanshi from "../assets/images/priyanshi.jpeg"
+import Aman from "../assets/images/Aman.jpeg"
+import Toshak from "../assets/images/Toshak.jpeg"
+import Varun from "../assets/images/Varun.jpeg"
+import Akash from "../assets/images/Akash.jpeg"
+import Sanat from "../assets/images/Sanat.jpg"
+import Ankush from "../assets/images/Ankush.jpg"
+import prakhar from "../assets/images/prakhar.jpeg"
+
 
 const locations = [
   {
@@ -26,50 +35,42 @@ const locations = [
 const users = [
   {
     name: 'Gabrielle Fernandez',
-    image:
-      'https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=600&w=600&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+    image:{priyanshi},
     position: 'Marketing Lead',
   },
   {
     name: 'Victória Silva',
-    image:
-      'https://images.generated.photos/vBRCiI_3UM4l40sU8s7fCwbJwzDwRTGpebzPkfHFsY4/rs:fit:512:512/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/ODgyMTAyLmpwZw.jpg',
+    image:{Aman},
     position: 'Back-end developer',
   },
   {
     name: 'Gabrielle Fernandez',
-    image:
-      'https://images.unsplash.com/photo-1549351512-c5e12b11e283?q=80&fm=jpg&crop=faces&fit=crop&h=600&w=600',
+    image:{Toshak},
     position: 'Sales',
   },
   {
     name: 'Sadie Lewis',
-    image:
-      'https://images.unsplash.com/photo-1485960994840-902a67e187c8?q=80&fm=jpg&crop=faces&fit=crop&h=600&w=600',
+    image:{Varun},
     position: 'Sales',
   },
   {
     name: 'Anubhav',
-    image:
-      'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&crop=faces&fit=crop&h=600&w=600',
+    image:{Akash},
     position: 'Marketing Lead',
   },
   {
     name: 'Deepika Ramesh',
-    image:
-      'https://images.generated.photos/lEhPUYYfYr9IOt13WZZGf2zbXZnCYdU1wUp7gcu_99c/rs:fit:512:512/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/Njg5NTQ5LmpwZw.jpg',
+    image:{Sanat},
     position: 'Front-end developer',
   },
   {
     name: 'Jordi Santiago',
-    image:
-      'https://images.pexels.com/photos/2269872/pexels-photo-2269872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image:{Ankush},
     position: 'Front-end developer',
   },
   {
     name: 'Kerim Fahri',
-    image:
-      'https://images.unsplash.com/photo-1456327102063-fb5054efe647?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=600&w=600',
+    image:{prakhar},
     position: 'Back-end developer',
   },
 ]
