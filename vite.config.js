008158aca99c0fed86bd009a3adbb0ac9c759399
KavyaPratap Singh <kavyapratapsingh221@gmail.com>
2025-04-20 21:54:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.JPG', '**/*.jpeg', '**/*.png'],
+  assetsInclude: ['**/*.JPG', '**/*.jpeg', '**/*.png','**/*.svg'],
   optimizeDeps: {
     include: [
       'swiper',
       'swiper/react',
-      'react-masonry-css'
+      'react-masonry-css',
+      'imagesloaded'
     ]
-  }
+  },
   build: {
     rollupOptions: {
       external: ['imagesloaded']
