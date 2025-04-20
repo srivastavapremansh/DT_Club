f3d99416b2c510b5215c719a093ce49b97132eea
KavyaPratap Singh <kavyapratapsingh221@gmail.com>
2025-04-21 02:33:18 +0530

    Update hackathon-landing-page.jsx
    made image path relative '../assets/coi/calofinnovation.jpg';

--- src/pages/hackathon-landing-page.jsx
+++ src/pages/hackathon-landing-page.jsx
@@ -328,7 +328,7 @@ export function HackathonLandingPage() {
               </div>
               <div className="flex items-center justify-center">
                 <img
-                  src="/src/assets/coi/calofinnovation.jpg"
+                  src= '../assets/coi/calofinnovation.jpg'
                   alt="Innovation Illustration"
                   width={500}
                   height={400}
